557930c8f1601472d8d2adcd6e149ac44792f43b
mrudul03 <mrudul.palvankar@gmail.com>
2016-02-05 12:01:38 +0530

    changef for annotation and dependencies

--- transactions-core-service/src/main/java/rest/core/TransactionsCoreServiceApplication.java
+++ transactions-core-service/src/main/java/rest/core/TransactionsCoreServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan
 public class TransactionsCoreServiceApplication {
 	
