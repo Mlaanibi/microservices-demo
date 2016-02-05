5c5f5b65917278d0ffe51e31d5298bdbcaaeb170
mrudul03 <mrudul.palvankar@gmail.com>
2016-02-05 12:00:39 +0530

    changed for annotations and dependencies

--- transactions-api-service/src/main/java/rest/api/TransactionsApiServiceApplication.java
+++ transactions-api-service/src/main/java/rest/api/TransactionsApiServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @EnableCircuitBreaker
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan
 public class TransactionsApiServiceApplication {
 	
