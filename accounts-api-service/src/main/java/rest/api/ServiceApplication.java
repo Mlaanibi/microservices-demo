de7d883af687964b193991df27a35cd134c98744
mrudul03 <mrudul.palvankar@gmail.com>
2016-02-05 11:40:10 +0530

    changed annotations

--- accounts-api-service/src/main/java/rest/api/ServiceApplication.java
+++ accounts-api-service/src/main/java/rest/api/ServiceApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @EnableCircuitBreaker
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
