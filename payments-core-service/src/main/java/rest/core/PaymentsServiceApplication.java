785fadd11dda47bf9953e5f88bec24605e227051
mrudul03 <mrudul.palvankar@gmail.com>
2016-02-05 11:56:33 +0530

    changed annotation

--- payments-core-service/src/main/java/rest/core/PaymentsServiceApplication.java
+++ payments-core-service/src/main/java/rest/core/PaymentsServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
