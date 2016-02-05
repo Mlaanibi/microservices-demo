6ab2e3a94aec2b2b90d95352848b52ba2ec654c0
mrudul03 <mrudul.palvankar@gmail.com>
2016-02-05 11:48:42 +0530

    changed annotations

--- accounts-composite-service/src/main/java/rest/composite/AccountCompositeApplication.java
+++ accounts-composite-service/src/main/java/rest/composite/AccountCompositeApplication.java
@@ -3,10 +3,10 @@ package rest.composite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
